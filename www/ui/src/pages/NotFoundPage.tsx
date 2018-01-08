d5717873212e653795215cda0ed2c9ceb8acdf1a
Jonathan Ingram <jonathan.ingram@digital.gov.au>
2018-01-08 11:19:43 +1100

    www: fix rogue "2" in 404 page title

--- www/ui/src/pages/NotFoundPage.tsx
+++ www/ui/src/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ interface Props {}
 const NotFoundPage: React.StatelessComponent<Props> = () => (
   <React.Fragment>
     <Helmet>
-      <title>404 page not found2</title>
+      <title>404 page not found</title>
     </Helmet>
     <h2>Page not found</h2>
     <p>That's all we know.</p>
