2c1890e5934a0c545725ad474f473a8fb4a012ee
Jonathan Ingram <jonathan.ingram@digital.gov.au>
2018-02-13 10:08:14 +1100

    www: b-cld can now hit cloud.gov.au API directly

--- cmd/wwwd/ci/build-ui-env-vars-b.cld.gov.au.sh
+++ cmd/wwwd/ci/build-ui-env-vars-b.cld.gov.au.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export REACT_APP_API_ENDPOINT="https://www.system.y.cld.gov.au"
+export REACT_APP_API_ENDPOINT="https://cloud.gov.au"
 export REACT_APP_META_ROBOTS="index, follow"
