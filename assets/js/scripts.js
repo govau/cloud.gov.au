86907160db5d171db8710a5feff82a7deb2f212b
Michael Tran <michael@dublue.com>
2019-09-30 14:10:17 +1000

    Actually add back to top things and separate sass variables

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,20 @@ $( document ).ready( function() {
     if ( $('#toc-container > ul').length > 0 ) {
         $('#sidebar > ul.au-link-list > li.active').append( $('div#toc-container > ul.au-link-list') );
 
+        $('#back-to-top a').on( 'click', function() {
+            $('html, body').animate({
+                    scrollTop: 0
+            }, 'fast');
+        });
+        $(window).scroll(function() {
+            if ( window.scrollY > $(window).height() ) {
+                    $('#back-to-top').addClass('visible');
+            }
+            else {
+                    $('#back-to-top').removeClass('visible');
+            }
+        });
+
         /*
         $('#toc-container').append(
             '<h2 class="au-sidenav__title hidden">On this page</h2>',
